ea4e710e47a82c295e813f7e7c70b6a356b164da
Your Name <you@example@example.com>
2026-06-24 14:42:46 +0100

    updated base line

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio3d/', 
   plugins: [react()],
   server: {
     port: 3000
